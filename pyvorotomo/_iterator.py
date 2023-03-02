efc68ab7e435479727c0018a0273780ae1959337
Robert Pickle <rpickle@gmail.com>
2023-03-03 10:06:30 +1100

    Update _iterator.py

--- pyvorotomo/_iterator.py
+++ pyvorotomo/_iterator.py
@@ -893,8 +893,9 @@ class InversionIterator(object):
             data_min = data.min(axis=0)
             data_max = data.max(axis=0)
             data_range = data_max - data_min
-            if data_range == 0:
-                data_range = 1e-9 # failsafe (RCP)
+            data_range += 1e-10
+            #if data_range == 0:
+            #    data_range = 1e-9 # failsafe (RCP)
             data_delta = data - data_min
             data = data_delta / data_range
 
@@ -1008,8 +1009,10 @@ class InversionIterator(object):
             data_min = data.min(axis=0)
             data_max = data.max(axis=0)
             data_range = data_max - data_min
-            if data_range == 0:
-                data_range = 1e-10 # failsafe
+            data_range += 1e-10
+            #if not data_range.all():
+                #data_range[data_range==0] = 1e-10
+                #data_range = 1e-10 # failsafe
             data_delta = data - data_min
             data = data_delta / data_range
 

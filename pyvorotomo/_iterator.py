fe46d861c24aa07e87e03898f35238d715b8bb70
Robert Pickle <rpickle@gmail.com>
2024-01-08 14:18:53 +1100

    final watermask fix
    couldn't use t/f mask in 3D

--- pyvorotomo/_iterator.py
+++ pyvorotomo/_iterator.py
@@ -2006,16 +2006,16 @@ class InversionIterator(object):
             handle = f"{phase}wave_model"
             model = getattr(self, handle)
             orig_model = model.values.copy()
-            watermask = model.values < 0.1 #model is in VELOCITY here
+            watermask = model.values <= 0.1
+            wateridx = np.where(watermask)
             values = np.power(model.values, -1) + values
             model.values = np.power(values, -1)
-            model.values[watermask] = orig_model #restore original low-velocity areas
+            model.values[watermask] = orig_model[wateridx] #restore original low-velocity areas
 
             handle = f"{phase}wave_variance"
             model = getattr(self, handle)
             model.values = variance
 
-        orig_model = None
         attrs = [f"{phase}wave_model"]
         self.synchronize(attrs=attrs)
 
